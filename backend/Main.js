32bdcb2ec3cc204db0654a9a292d72ff31ab8191
Chandu Gowda <chandubrogowda12.com>
2025-01-10 14:10:44 +0530

    FIRST BACKEND CODE [ENV FILE , FAKE DATA ]

--- backend/Main.js
+++ backend/Main.js
@@ -1,7 +1,28 @@
-const express = require('express');
-const app  = express();
+const express = require("express");
+const app = express();
 app.use(express.json());
+const J_Data = require("./data/data");
+const dotenv = require("dotenv");
 
-app.listen(()=>{
-    console.log("Running");
-})
+dotenv.config(); //MAKING USE OF THE ENV DATA
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+// app.get("/api/J_Data", (req, res) => {
+//   res.send(J_Data)
+// });
+
+app.get("/api/J_Data/:id", (req, res) => {
+  const id = req.params.id;
+  const Req_data = J_Data.find((data) => data._id === id);
+  res.send(Req_data);
+});
+
+//CRAETED THE SERVER
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
